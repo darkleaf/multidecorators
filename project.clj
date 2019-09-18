fe07124966bd74830c5a89c6d3b1e2fb6c611299
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-09-18 17:55:33 +0400

    1.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject darkleaf/multidecorators "1.0.3"
+(defproject darkleaf/multidecorators "1.0.4"
   :description "Like multimethods but multidecorators."
   :url "https://github.com/darkleaf/multidecorators/"
   :license {:name "Unlicense"
